c8c669dd890bccb0273975219c32544699d0b72d
ugurcanozcelik <44464584+ugurcanozcelik@users.noreply.github.com>
2019-12-19 20:30:13 +0300

    deneme1

---
+++ finalders5/src/main/java/org/medipol/yazilimaraclari/finalders5/Finalders5Application.java
@@ -0,0 +1,13 @@
+package org.medipol.yazilimaraclari.finalders5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalders5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finalders5Application.class, args);
+	}
+
+}
